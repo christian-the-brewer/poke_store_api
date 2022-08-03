8ee68a9d29ed839077b36a47eec9afb9190fd604
jadenruplal <jadenruplal@gmail.com>
2022-08-03 19:26:34 -0400

    v1 cartRoute JR

---
+++ app/models/cart.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const cartSchema = new mongoose.Schema(
+	{
+		products:[ {
+            name: String,
+            price: Number,
+            quantity: Number
+        } ],
+		owner: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User',
+			required: true,
+		},
+	},
+	{
+		timestamps: true,
+	}
+)
+
+module.exports = mongoose.model('Cart', cartSchema)
