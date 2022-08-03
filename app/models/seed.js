39badd5d46593634be6a4467c3d7d9480acb9f36
christian-the-brewer <brewer.christiang@gmail.com>
2022-08-03 19:43:00 -0400

    Adds seed file

---
+++ app/models/seed.js
@@ -0,0 +1,65 @@
+//seed files runs with `npm run seed`
+//deletes current databse and and replaces with only what is in seed database
+//TODO make it so it only deletes bikes without owner
+
+const mongoose = require('mongoose')
+const Item = require('./item')
+const db = require('../../config/db')
+
+const stockPokeMart = [
+    {
+        name: 'Bulbasaur',
+        image: 'https://img.pokemondb.net/artwork/avif/bulbasaur.avif',
+        cost: 500,
+        description: 'Bulbasaur is a Grass/Poison type Pokémon introduced in Generation 1. It is known as the Seed Pokémon.',
+        type: 'Grass',
+        stock: 10
+    },
+    {
+        name: 'Charmander',
+        image: 'https://img.pokemondb.net/artwork/avif/charmander.avif',
+        cost: 500,
+        description: 'Charmander is a Fire type Pokémon introduced in Generation 1. It is known as the Lizard Pokémon.',
+        type: 'Fire',
+        stock: 10
+    },
+    {
+        name: 'Squirtle',
+        image: 'https://img.pokemondb.net/artwork/avif/squirtle.avif',
+        cost: 500,
+        description: 'Squirtle is a Water type Pokémon introduced in Generation 1. It is known as the Tiny Turtle Pokémon.',
+        type: 'Water',
+        stock: 10
+    },
+
+]
+
+//connect to database
+mongoose.connect(db, {
+    useNewUrlParser: true
+})
+    .then(() => {
+        // first we remove all of the motorcycles
+        Item.deleteMany({ owner: null })
+            .then(deletedItems => {
+                console.log('deletedItems', deletedItems)
+                // the next step is to use our startMotorcycles array to create our seeded motorcycles
+                Item.create(stockPokeMart)
+                    .then(newItems => {
+                        console.log('the new items', newItems)
+                        mongoose.connection.close()
+                    })
+                    .catch(error => {
+                        console.log(error)
+                        mongoose.connection.close()
+                    })
+            })
+            .catch(error => {
+                console.log(error)
+                mongoose.connection.close()
+            })
+    })
+    .catch(error => {
+        console.log(error)
+        mongoose.connection.close()
+    })
