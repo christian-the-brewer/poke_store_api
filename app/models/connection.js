6f0f6dc3cef8fb96e3828e800c6f4bd891e89a73
christian-the-brewer <brewer.christiang@gmail.com>
2022-08-10 18:45:28 -0400

    Updates connection js for deployment

---
+++ app/models/connection.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+// Fire off the connection to Mongo DB
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+
+mongoose.connection.on('connected', () => {
+    console.log(`Mongoose connected to ${mongoose.connection.host}:${mongoose.connection.port}`);
+});
+
+mongoose.connection.on("error", (err) => {
+    console.log("Could not connect to MongoDB!", err);
+});
